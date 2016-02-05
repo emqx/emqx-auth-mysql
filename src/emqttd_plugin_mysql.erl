ff5ff86677737bdf72d3250b9ae2a43474fe735e
Feng <ery.lee@gmail.com>
2016-02-05 14:15:24 +0800

    Licensed under the Apache, Version 2.0

--- src/emqttd_plugin_mysql.erl
+++ src/emqttd_plugin_mysql.erl
@@ -1,28 +1,21 @@
-%%%-----------------------------------------------------------------------------
-%%% Copyright (c) 2012-2016 eMQTT.IO, All Rights Reserved.
-%%%
-%%% Permission is hereby granted, free of charge, to any person obtaining a copy
-%%% of this software and associated documentation files (the "Software"), to deal
-%%% in the Software without restriction, including without limitation the rights
-%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%%% copies of the Software, and to permit persons to whom the Software is
-%%% furnished to do so, subject to the following conditions:
-%%%
-%%% The above copyright notice and this permission notice shall be included in all
-%%% copies or substantial portions of the Software.
-%%%
-%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-%%% SOFTWARE.
-%%%-----------------------------------------------------------------------------
-%%% @doc MySQL Authentication/ACL Application
-%%%
-%%% @author Feng Lee <feng@emqtt.io>
-%%%-----------------------------------------------------------------------------
+%%--------------------------------------------------------------------
+%% Copyright (c) 2012-2016 Feng Lee <feng@emqtt.io>.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+%% @doc MySQL Authentication/ACL Application
+%% @author Feng Lee <feng@emqtt.io>
 -module(emqttd_plugin_mysql).
 
 -behaviour(application).
@@ -35,9 +28,9 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%%%=============================================================================
-%%% Application callbacks
-%%%=============================================================================
+%%--------------------------------------------------------------------
+%% Application callbacks
+%%--------------------------------------------------------------------
 
 start(_StartType, _StartArgs) ->
     {ok, AuthSql}  = application:get_env(?MODULE, authquery),
@@ -59,9 +52,9 @@ prep_stop(State) ->
 stop(_State) ->
     ok.
 
-%%%=============================================================================
-%%% Supervisor callbacks(Dummy)
-%%%=============================================================================
+%%--------------------------------------------------------------------
+%% Supervisor callbacks(Dummy)
+%%--------------------------------------------------------------------
 
 init([]) ->
 
