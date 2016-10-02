2b938712620b957cb73d1092e5754980dab9d88d
Feng <ery.lee@gmail.com>
2016-10-02 12:23:58 +0800

    BUILD_DEPS

--- src/emqttd_auth_mysql_app.erl
+++ src/emqttd_auth_mysql_app.erl
@@ -34,13 +34,13 @@ start(_StartType, _StartArgs) ->
     {ok, Sup} = emqttd_auth_mysql_sup:start_link(),
     if_enabled(auth_query, fun(AuthQuery) ->
         SuperQuery = parse_query(gen_conf:value(?APP, superquery, undefined)),
-        {ok, HashType}  = gen_conf:value(?APP, password_hash),
-        AuthEnv = {parse_query(AuthQuery), SuperQuery, HashType},
+        {ok, HashType} = gen_conf:value(?APP, password_hash),
+        AuthEnv = {AuthQuery, SuperQuery, HashType},
         emqttd_access_control:register_mod(auth, emqttd_auth_mysql, AuthEnv)
     end),
     if_enabled(acl_query, fun(AclQuery) ->
         {ok, AclNomatch} = gen_conf:value(?APP, acl_nomatch),
-        AclEnv = {parse_query(AclQuery), AclNomatch},
+        AclEnv = {AclQuery, AclNomatch},
         emqttd_access_control:register_mod(acl, emqttd_acl_mysql, AclEnv)
     end),
     {ok, Sup}.
@@ -57,9 +57,9 @@ stop(_State) ->
 %% Internal function
 %%--------------------------------------------------------------------
 
-if_enabled(Key, Fun) ->
-    case gen_conf:value(?APP, Key) of
-        {ok, Query} -> Fun(Query);
+if_enabled(Cfg, Fun) ->
+    case gen_conf:value(?APP, Cfg) of
+        {ok, Query} -> Fun(parse_query(Query));
         undefined   -> ok
     end.
 
