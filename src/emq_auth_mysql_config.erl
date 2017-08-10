e35f8d54e0a0ce8cf1a00d4584342b5f76402064
turtled <turtled@emqtt.io>
2017-08-10 17:33:30 +0800

    Add CLI set/get env

---
+++ src/emq_auth_mysql_config.erl
@@ -0,0 +1,141 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2013-2017 EMQ Enterprise, Inc. (http://emqtt.io)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module (emq_auth_mysql_config).
+
+-include("emq_auth_mysql.hrl").
+
+-export ([register/0, unregister/0]).
+
+%%--------------------------------------------------------------------
+%% API
+%%--------------------------------------------------------------------
+register() ->
+    clique_config:load_schema([code:priv_dir(?APP)], ?APP),
+    register_formatter(),
+    register_config().
+
+unregister() ->
+    unregister_formatter(),
+    unregister_config(),
+    clique_config:unload_schema(?APP).
+
+%%--------------------------------------------------------------------
+%% Get ENV Register formatter
+%%--------------------------------------------------------------------
+register_formatter() ->
+    [clique:register_formatter(cuttlefish_variable:tokenize(Key), 
+     fun formatter_callback/2) || Key <- keys()].
+
+formatter_callback([_, _, "server"], Params) ->
+    lists:concat([proplists:get_value(host, Params), ":", proplists:get_value(port, Params)]);
+formatter_callback([_, _, "pool"], Params) ->
+    proplists:get_value(pool_size, Params);
+formatter_callback([_, _, "database"], Params) ->
+    proplists:get_value(database, Params);
+formatter_callback([_, _, "username"], Params) ->
+    proplists:get_value(user, Params);
+formatter_callback([_, _, "password"], Params) ->
+    proplists:get_value(password, Params);
+formatter_callback([_, _, "password_hash"], Params) when is_atom(Params) ->
+    Params;
+formatter_callback([_, _, "password_hash"], Params) when is_tuple(Params) ->
+    format(tuple_to_list(Params)).
+
+%%--------------------------------------------------------------------
+%% UnRegister formatter
+%%--------------------------------------------------------------------
+unregister_formatter() ->
+    [clique:unregister_formatter(cuttlefish_variable:tokenize(Key)) || Key <- keys()].
+
+%%--------------------------------------------------------------------
+%% Set ENV Register Config
+%%--------------------------------------------------------------------
+register_config() ->
+    Keys = keys(),
+    [clique:register_config(Key , fun config_callback/2) || Key <- Keys],
+    clique:register_config_whitelist(Keys, ?APP).
+
+config_callback([_, _, "server"], Value0) ->
+    {Host, Port} = parse_servers(Value0),
+    {ok, Env} = application:get_env(?APP, server),
+    Env1 = lists:keyreplace(hosts, 1, Env, {host, Host}),
+    Env2 = lists:keyreplace(port, 1, Env1, {port, Port}),
+    application:set_env(?APP, server, Env2),
+    " successfully\n";
+config_callback([_, _, "pool"], Value) ->
+    {ok, Env} = application:get_env(?APP, server),
+    application:set_env(?APP, server, lists:keyreplace(pool_size, 1, Env, {pool_size, Value})),
+    " successfully\n";
+config_callback([_, _, "username"], Value) ->
+    {ok, Env} = application:get_env(?APP, server),
+    application:set_env(?APP, server, lists:keyreplace(username, 1, Env, {username, Value})),
+    " successfully\n";
+config_callback([_, _, "password"], Value) ->
+    {ok, Env} = application:get_env(?APP, server),
+    application:set_env(?APP, server, lists:keyreplace(password, 1, Env, {password, Value})),
+    " successfully\n";
+config_callback([_, _, "database"], Value) ->
+    {ok, Env} = application:get_env(?APP, server),
+    application:set_env(?APP, server, lists:keyreplace(database, 1, Env, {database, Value})),
+    " successfully\n";
+config_callback([_, _, "password_hash"], Value0) ->
+    Value = parse_password_hash(Value0),
+    application:set_env(?APP, password_hash, Value),
+    " successfully\n".
+
+%%--------------------------------------------------------------------
+%% UnRegister config
+%%--------------------------------------------------------------------
+unregister_config() ->
+    Keys = keys(),
+    [clique:unregister_config(Key) || Key <- Keys],
+    clique:unregister_config_whitelist(Keys, ?APP).
+
+%%--------------------------------------------------------------------
+%% Internal Functions
+%%--------------------------------------------------------------------
+keys() ->
+    ["auth.mysql.server",
+     "auth.mysql.pool",
+     "auth.mysql.username",
+     "auth.mysql.password",
+     "auth.mysql.database",
+     "auth.mysql.password_hash"].
+
+format(Value) ->
+    format(Value, "").
+format([Head], Acc) ->
+    lists:concat([Acc, Head]);
+format([Head | Tail], Acc) ->
+    format(Tail, Acc ++ lists:concat([Head, ","])).
+
+parse_password_hash(Value) ->
+    case string:tokens(Value, ",") of
+          [Hash]           -> list_to_atom(Hash);
+          [Prefix, Suffix] -> {list_to_atom(Prefix), list_to_atom(Suffix)};
+          [Hash, MacFun, Iterations, Dklen] -> {list_to_atom(Hash), 
+                                                list_to_atom(MacFun), 
+                                                list_to_integer(Iterations), 
+                                                list_to_integer(Dklen)};
+          _                -> plain
+    end.
+
+parse_servers(Value) ->
+    case string:tokens(Value, ":") of
+        [Domain]       -> {Domain, 3306};
+        [Domain, Port] -> {Domain, list_to_integer(Port)}
+    end.
